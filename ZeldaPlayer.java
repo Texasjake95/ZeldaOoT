0b8ee7401e2ba501a66b7c57629d6d98a1fc511f
Texasjake95 <texasjake95@gmail.com>
2012-08-01 20:42:35 -0500

    Forgot to remove old code that I commented out

--- ZeldaPlayer.java
+++ ZeldaPlayer.java
@@ -24,26 +24,3 @@ public class ZeldaPlayer extends PlayerBase
 		this.Mana.writeNBT(par1NBTTagCompound);
 	}
 }
-
-/*
-
-public Player(World par1World) {
-	super(par1World);
-	
-}
-public void readEntityFromNBT(NBTTagCompound par1NBTTagCompound)
-{
-	super.readEntityFromNBT(par1NBTTagCompound);
-	this.Mana.readNBT(par1NBTTagCompound);
-}
-public void writeEntityToNBT(NBTTagCompound par1NBTTagCompound)
-{
-	super.writeEntityToNBT(par1NBTTagCompound);
-	this.Mana.writeNBT(par1NBTTagCompound);
-}
-@Override
-public void func_6420_o() {
-	// TODO Auto-generated method stub
-	
-}
-*/
